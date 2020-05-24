18df8fb02139e062a4fd75b954a6b1fab95d0766
chiragdoctor <chirag.doctor@gmail.com>
2020-05-24 17:44:28 +0100

    add get todos route

--- routes/todo/index.js
+++ routes/todo/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const Todo = require('../../db/models/todo');
 
 router.get('/', (req, res) => {
-    
-})
-
-
+    Todo.find({})
+    .then((err, todos) => {
+        if(err) {
+            res.send(err);
+        }
+        res.send(todos);
+    })
+});
 
 
 module.exports = router;
