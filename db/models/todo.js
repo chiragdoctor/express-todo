2025cef4de7a119a3194abc78c69e645cf9f4bf7
chiragdoctor <chirag.doctor@gmail.com>
2020-05-24 17:35:39 +0100

    create todo schema

--- db/models/todo.js
+++ db/models/todo.js
@@ -2,5 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const todoSchema = new Schema({
-    
+    title: {
+        type: String
+    },
+    description: {
+        type: String
+    }
 });
+
+module.exports = new mongoose.Model('todo', todoSchema, 'todos');
